bd3b5dd3588f8d5e561365ddfec6e99773a2e287
camonday <arkadiakowalczyk@gmail.com>
2023-01-25 14:49:06 +0100

    dodane graficzne interfejsy

--- src/HelloKlient.java
+++ src/HelloKlient.java
@@ -1,6 +1,8 @@
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,6 +14,18 @@ public class HelloKlient {
     private boolean zmiana;
     private String bufforek, quest;
     private JLabel imie;
+
+    private JLabel jadlospis;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton zmienDaneButton;
+    private JButton jadłospisButton;
+    private JButton mojeDaneButton;
+    private JLabel tresc_komunikatu;
 
     public HelloKlient(int id_klienta) {
         moje_id=id_klienta;
@@ -25,8 +39,65 @@ public class HelloKlient {
         }
         imie.setVisible(true);
 
+        zmienDaneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!textField1.getText().isEmpty() && !textField2.getText().isEmpty() && !textField3.getText().isEmpty() && !textField4.getText().isEmpty() && !textField5.getText().isEmpty() && !textField6.getText().isEmpty()){
+                    quest = "CALL `mydb2`.``zmienDaneKlient`(?,?,?,?,?,?,?);\n";
+                    Poloczenie6Variable(quest, textField1.getText(), textField2.getText(), Integer.parseInt(textField3.getText()), Float.parseFloat(textField4.getText()), textField5.getText(), textField6.getText());
+                }
+            }
+        });
+
+        jadłospisButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quest = "CALL `mydb2`.``sprawdzJadlospisKlient`(?);\n";
+                Poloczenie0Variable(quest);
+            }
+        });
+
+        mojeDaneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quest = "CALL `mydb2`.``przejrzyjDaneKlient`(?);\n";
+                Poloczenie0Variable(quest);
+            }
+        });
     }
 
+    void Poloczenie0Variable(String query_){
+        try {
+            Connection conn = Main.otworzPoloczenie();
+
+            try (PreparedStatement Query = conn.prepareStatement(query_)){
+                conn.setAutoCommit(false);
+                Query.setInt(1, moje_id);
+
+                ResultSet rs = Query.executeQuery();
+                conn.commit();
+                while (rs.next()) {
+                    OdpowiedzDoOkienka(rs);
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    conn.rollback();
+                } catch (SQLException excep) {
+                    excep.printStackTrace();
+                }
+            }
+
+            Main.zamknijPoloczenie(conn);
+        } catch (SQLException e) {
+            System.out.println("Nie ok");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problem ze sterownikiem");
+        }
+
+
+    }
     void Poloczenie1Variable(String query_, int var1){
         try {
             Connection conn = Main.otworzPoloczenie();
@@ -60,7 +131,40 @@ public class HelloKlient {
 
     }
 
-    void OdpowiedzDoBufforka(@NotNull ResultSet rs){
+    void Poloczenie6Variable(String query_, String var1, String var2, int var3, float var4, String var5, String var6 ){
+        try {
+            Connection conn = Main.otworzPoloczenie();
+
+            try (PreparedStatement Query = conn.prepareStatement(query_)){
+                conn.setAutoCommit(false);
+                Query.setInt(1, moje_id);
+                Query.setString(2, var1);
+                Query.setString(3, var2);
+                Query.setInt(4, var3);
+                Query.setFloat(5, var4);
+                Query.setString(6, var5);
+                Query.setString(7, var6);
+                conn.commit();
+            }catch (SQLException e) {
+                e.printStackTrace();
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    conn.rollback();
+                } catch (SQLException excep) {
+                    excep.printStackTrace();
+                }
+            }
+
+            Main.zamknijPoloczenie(conn);
+        } catch (SQLException e) {
+            System.out.println("Nie ok");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problem ze sterownikiem");
+        }
+
+
+    }
+    void OdpowiedzDoBufforka(/*@NotNull*/ ResultSet rs){
         StringBuilder tekstB= new StringBuilder();
         for (int i=1; ;i++){
             try {
@@ -68,6 +172,20 @@ public class HelloKlient {
                 tekstB.append(rs.getString(i));
             }catch(SQLException e){
                 bufforek = (tekstB.toString());
+                zmiana = true;
+                break;
+            }
+        }
+    }
+
+    void OdpowiedzDoOkienka(@NotNull ResultSet rs){
+        StringBuilder tekstB= new StringBuilder();
+        for (int i=1; ;i++){
+            try {
+                tekstB.append("     ");
+                tekstB.append(rs.getString(i));
+            }catch(SQLException e){
+                tresc_komunikatu.setText(tekstB.toString());
                 zmiana = true;
                 break;
             }
--- src/helloDostawca.java
+++ src/helloDostawca.java
@@ -1,8 +1,80 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class helloDostawca {
-    public JPanel panel1;
+     JPanel panel1;
     private JPanel odpowiedzPane;
     private JTextField textField1;
     private JButton gdzieZawieźćPaczkęButton;
+    private JLabel tresc;
+    private String quest;
+    private int ID;
+
+    public helloDostawca(){
+        tresc.setVisible(true);
+        odpowiedzPane.setVisible(false);
+
+        gdzieZawieźćPaczkęButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!textField1.getText().isEmpty()){
+                    ID = Integer.parseInt(textField1.getText());
+                    quest = "CALL `mydb2`.`gdzieZawiezcPaczkeDostawca`(?);\n";
+                    Poloczenie1Variable(quest, ID);
+                    odpowiedzPane.setVisible(true);
+                }
+            }
+        });
+    }
+
+    void Poloczenie1Variable(String query_, int var1){
+        try {
+            Connection conn = Main.otworzPoloczenie();
+
+            try (PreparedStatement Query = conn.prepareStatement(query_)){
+                conn.setAutoCommit(false);
+                Query.setInt(1, var1);
+
+                ResultSet rs = Query.executeQuery();
+                conn.commit();
+                while (rs.next()) {
+                    OdpowiedzDoOkienka(rs);
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    conn.rollback();
+                } catch (SQLException excep) {
+                    excep.printStackTrace();
+                }
+            }
+
+            Main.zamknijPoloczenie(conn);
+        } catch (SQLException e) {
+            System.out.println("Nie ok");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problem ze sterownikiem");
+        }
+
+
+    }
+
+    void OdpowiedzDoOkienka(/*@NotNull*/ ResultSet rs){
+        StringBuilder tekstB= new StringBuilder();
+        for (int i=1; ;i++){
+            try {
+                tekstB.append("     ");
+                tekstB.append(rs.getString(i));
+            }catch(SQLException e){
+                tresc.setText(tekstB.toString());
+                break;
+            }
+        }
+    }
 }
